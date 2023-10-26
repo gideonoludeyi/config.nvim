457a58898dcc19db35865569c9fb80e92eb49c6d
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2023-10-26 00:53:28 -0400

    set AerialToggle keymap

--- init.lua
+++ init.lua
@@ -561,5 +561,8 @@ cmp.setup {
   },
 }
 
+-- [[ Config aerial.nvim ]]
+vim.keymap.set({ "n", "v" }, "<leader>ta", "<cmd>AerialToggle<CR>", { desc = "[T]oggle [A]erial map" })
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
