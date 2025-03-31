5dc89c436f30ff9f9c8318c8b57a395955cb1f87
gideonoludeyi <49620086+gideonoludeyi@users.noreply.github.com>
2025-03-31 00:21:55 -0400

    update

--- lua/custom/lspconfig/init.lua
+++ lua/custom/lspconfig/init.lua
@@ -111,6 +111,7 @@ local mason_lspconfig = require('mason-lspconfig')
 
 mason_lspconfig.setup {
   ensure_installed = vim.tbl_keys(servers),
+  automatic_installation = true,
 }
 
 local default_config = {
